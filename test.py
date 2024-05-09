b06b62707b3818dff4f75aa16d26ae9b63d6ea3a
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-05-09 23:56:57 +0530

    gradio

---
+++ test.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def predict(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
