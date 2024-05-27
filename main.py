8820929dcdc1178410d1316fb79024fecdf58609
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-05-27 22:34:09 +0530

    Updated

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Forecast App')
+st.title('Crypto Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+crypto = ('BTC-USD', 'ETH-USD', 'ICP-USD', 'SOL-USD')
+selected_crypto = st.selectbox('Select dataset for prediction', crypto)
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
 
-@st.cache
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
@@ -27,7 +27,7 @@ def load_data(ticker):
 
 	
 data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
+data = load_data(selected_crypto)
 data_load_state.text('Loading data... done!')
 
 st.subheader('Raw data')
@@ -36,8 +36,8 @@ st.write(data.tail())
 # Plot raw data
 def plot_raw_data():
 	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="crypto_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="crypto_close"))
 	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 	
